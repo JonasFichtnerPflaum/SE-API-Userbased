8100acb12adea7f186c77f609a053c592b8526a5
JonasFichtnerPflaum <jonas.fichtnerpflaum@gmail.com>
2023-05-26 13:38:06 +0200

    test

---
+++ SE-API-Userbased/src/main/kotlin/com/example/SEAPIUserbased/SeApiUserbasedApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SEAPIUserbased
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeApiUserbasedApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeApiUserbasedApplication>(*args)
+}
---
+++ SE-API-Userbased/src/test/kotlin/com/example/SEAPIUserbased/SeApiUserbasedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SEAPIUserbased
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeApiUserbasedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
